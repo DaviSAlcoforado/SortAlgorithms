1adee67b17ccb6291db6017ce1cdbe8673003ee7
davisa6 <48020809+davisa6@users.noreply.github.com>
2019-10-22 17:04:00 -0300

    Add files via upload

--- bubblesort.c
+++ bubblesort.c
@@ -4,6 +4,9 @@
 #include <time.h>
 
 int main(){
+    clock_t Ticks[2];
+    Ticks[0] = clock();
+
     int i, j;
     int array[] = {1,3,5,6,2,4,9,45,321,1}; 
     int temp;
@@ -12,7 +15,7 @@ int main(){
      for (i=0; i<(tamanho-1);i++)
      {
         for (j = 0; j<(tamanho-1); j++){
-                if (array[j]>array[j+1])
+                if (array[j+1]>array[j])
                 {
                  temp = array[j];
                  array[j] = array[j+1];
@@ -24,11 +27,10 @@ int main(){
     for(j=0;j<(tamanho);j++){
         printf("%d ",array[j]);
     }
-    clock_t Ticks[2];
-    Ticks[0] = clock();
+
     Ticks[1] = clock();
     double Tempo = (Ticks[1] - Ticks[0]) * 1000.0 / CLOCKS_PER_SEC;
     printf("\nTempo gasto: %g ms.", Tempo);
     getchar();
     return 0;
-  }
+}
