f614a3dd452a67d9ed873542bc01d7a640676f40
Daví Silva <48020809+davisa6@users.noreply.github.com>
2019-11-24 19:44:13 -0300

    Update insertionsort.c

--- insertionsort.c
+++ insertionsort.c
@@ -8,23 +8,18 @@ int main(){
     Ticks[0] = clock();
 
     int i, j;
-    int array[] = {1,3,5,6,2,4,9,45,321,1};
+    int array[] = {15,84,75,21,36,47,95,32,14,20,1,3,2,8,4,6};
     int temp;
     int tamanho = sizeof(array)/sizeof(int);
 
-     for (i=0; i<(tamanho-1);i++)
+     for (i=0; i<(tamanho);i++)
      {
-        for (j = 0; j<(tamanho-1); j++){
-                if (array[j]>array[j+1])
-                {
-                 temp = array[j];
-                 array[j] = array[j+1];
-                 array[j+1] = temp;
-                 if (&array[j-1]!=NULL && array[j] < array[j-1]){
+        for (j = 1; j<(tamanho); j++){
+                 if (array[j] < array[j-1]){
                    temp = array[j];
                    array[j] = array[j-1];
                    array[j-1] = temp;
-                 }
+                 
                 }
                 }
               }
