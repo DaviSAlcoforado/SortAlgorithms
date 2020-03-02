1659973d083cd71e589163a98f904eb170c3a06f
Daví Silva <48020809+davisa6@users.noreply.github.com>
2020-03-02 12:08:08 -0300

    Add files via upload

---
+++ exerfogo.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    int array[10];
+    for (int i = 0; i<10; i++){
+        scanf("%d",&array[i]);
+    }
+
+    int soma = 0;
+    int maior_soma = 0;
+    int size = 10;
+    int contador = 0;
+        for (int j = 0; j<size; j++){
+                
+            for (int i = 0; i<size; i++){
+                soma += array[i];
+    
+        }
+                if (soma > maior_soma)
+                    maior_soma = soma;
+            soma = 0;
+        }
+    printf("Maior soma: %d",maior_soma);
+    return 0;
+}
